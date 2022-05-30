86d4789dff45c73740390cd0501e6feb3a23c9ff
shivambais <shivambais.sb@gmail.com>
2022-05-30 16:49:20 +0530

    changes2

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -11,19 +11,49 @@ import { Text,Box,Image, Stack, Heading, Tag, TagLabel} from '@chakra-ui/react'
 //   ModalCloseButton,
 // } from '@chakra-ui/react'
 
-const Product = () => {
+const Product = ({items}) => {
 
+  // items.map((item)=>{
+  //   return <Stack data-cy="product">
+  //   <Image data-cy="product-image" />
+  //   <Text data-cy="product-category"></Text>
+  //   <Tag>
+  //     <TagLabel data-cy="product-gender"></TagLabel>
+  //   </Tag>
+  //   <Heading data-cy="product-title"></Heading>
+  //   <Box data-cy="product-price"></Box>
+  // </Stack>
+  // })
+
+  // return (
+  //   <Stack data-cy="product">
+  //     <Image data-cy="product-image" />
+  //     <Text data-cy="product-category"></Text>
+  //     <Tag>
+  //       <TagLabel data-cy="product-gender"></TagLabel>
+  //     </Tag>
+  //     <Heading data-cy="product-title"></Heading>
+  //     <Box data-cy="product-price"></Box>
+  //   </Stack>
+  // );
 
   return (
-    <Stack data-cy="product">
-      <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
-      <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
-      </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
-    </Stack>
+    <div>
+
+       {{items}.map((item)=>{
+        return <Stack data-cy="product">
+        <Image data-cy="product-image" src={item.imageSrc}/>
+        <Text data-cy="product-category">{item.category}</Text>
+        <Tag>
+          <TagLabel data-cy="product-gender"> {item.gender}</TagLabel>
+        </Tag>
+        <Heading data-cy="product-title">{item.title}</Heading>
+        <Box data-cy="product-price">{item.price}</Box>
+      </Stack>
+      })}
+
+    </div>
+      
   );
 };
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useSyncExternalStore,useEffect } from "react";
 import AddProduct from "./AddProduct"
 import Product from "./Product"
 import Pagination from "./Pagination"
@@ -6,16 +6,26 @@ import Pagination from "./Pagination"
 import {Flex,Grid } from '@chakra-ui/react'
 
 const Products = () => {
-  // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  
+  const [items, setItems] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/products")
+    .then(res=>{return res.json()})
+    .then(res=>{setItems(res)})
+  },[])
+
+
 
   return (
     <Flex>
       {/*  AddProduct */}
       <AddProduct/>
-      <Grid>{/* List of Products */}</Grid>
+      <Grid>{/* List of Products */}
+        <Product items={items}/>
+      </Grid>
       {/* Pagination */}
+      
       <Pagination/>
     </Flex>
   );
